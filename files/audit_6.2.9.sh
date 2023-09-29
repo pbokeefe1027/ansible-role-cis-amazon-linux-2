5c53ab5e19335888d8f732931cf2c68a3e1c26c9
pbokeefe1027 <paul.b.okeefe@gmail.com>
2023-09-29 07:17:29 -0500

    fix directory ownership per Siva

--- files/audit_6.2.9.sh
+++ files/audit_6.2.9.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cat /etc/passwd | awk -F: '{ print $1 " " $3 " " $6 }' | while read user uid dir; do
-  if [ $uid -ge 500 -a -d "$dir" -a $user != "nfsnobody" ]; then
+  if [ $uid -ge 500 -a -d "$dir" -a $user != "nfsnobody" -a $user != "sssd" -a $user != "polkitd" -a $user != "systemd-coredump" -a $user != "nobody" ]; then
   owner=$(stat -L -c "%U" "$dir")
     if [ "$owner" != "$user" ]; then
       echo "$dir:$user:$owner"
